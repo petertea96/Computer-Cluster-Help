ea38732c7e606499186d718c3e0617a0f81edc21
PeterTea <petertea96@gmail.com>
2018-12-07 06:18:37 -0500

    New bash script

--- yourjob.sh
+++ yourjob.sh
@@ -2,6 +2,8 @@
 #SBATCH --job-name=myjobname
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ptea035@uottawa.ca
+#SBATCH --output=STD.out
+#SBATCH --error=STD.err
 #SBATCH -c 1 # Number of CPUS requested (default is 1 CPU).
 #SBATCH --mem=8mb # Memory requested in megabytes (default is 1024 MB).
 #SBATCH -t 0-0:0:5 # How long you expect your job to run for (default is 3 hours).
