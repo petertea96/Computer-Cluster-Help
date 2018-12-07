edd387b57b4ad374fd0387530d70f23d0f6db04f
Peter Tea <40365241+petertea96@users.noreply.github.com>
2018-12-07 06:40:54 -0500

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=arrayjob
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=ptea035@uottawa.ca
+#SBATCH --mail-user=email@uottawa.ca
 #SBATCH --output=arrayJob_%A_%a.out
 #SBATCH --error=arrayJob_%A_%a.err
 #SBATCH --array=1-16
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-9:0:0								# days-hours:minutes:seconds      # How long you expect your job to run for (default is 3 hours).
-#SBATCH --mem=8GB									# Memory requested in megabytes (default is 1024 MB).
+#SBATCH --time=0-9:0:0	# days-hours:minutes:seconds; How long you expect your job to run for (default is 3 hours).
+#SBATCH --mem=8GB	#Memory requested in megabytes (default is 1024 MB).
 
 # commands for your job go here
 
