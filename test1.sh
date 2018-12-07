0340bb0a899ad48951dad1384338090337e8a6f5
Peter Tea <40365241+petertea96@users.noreply.github.com>
2018-12-07 06:37:45 -0500

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -6,9 +6,9 @@
 #SBATCH --error=STD.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1							# Number of CPUS requested (default is 1 CPU).
-#SBATCH --time=0-1:0:0			  					# days-hours:minutes:seconds      # How long you expect your job to run for (default is 3 hours).
-#SBATCH --mem=2GB									# Memory requested in megabytes (default is 1024 MB).
+#SBATCH --cpus-per-task=1	#Number of CPUS requested (default is 1 CPU).
+#SBATCH --time=0-1:0:0	# days-hours:minutes:seconds; How long you expect your job to run for (default is 3 hours).
+#SBATCH --mem=2GB	# Memory requested in megabytes (default is 1024 MB).
 
 
 
